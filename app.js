b584047565fb0a901be91f34898b650f64a45d2f
sangeetha-hs <118202947+sangeetha-hs@users.noreply.github.com>
2023-12-28 05:48:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -35,11 +35,11 @@ const RestaurantCard = (props) => {
   } =resData?.info;
   return (
     <div className="res-card" style={{ backgroundColor: '#f0f0f0' }}>
-      <img className="res-logo"  src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/xqwpuhgnsaf18te7zvtv" + cloudinaryImageId} />
+      <img className="res-logo"  src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId} />
       <h3>{name}</h3>
       <h3>{cuisines.join(",")}</h3> 
       <h3>{avgRating}stars</h3>
-      <h4>{costForTwo/100} For Two</h4>
+      <h4>₹{costForTwo/100} For Two</h4>
       <h4>{deliveryTime} minutes</h4>
     </div>
   )
